1c9b0fbe7fbdeb2f5f0f4b8bbec4783f4ef61c4c
rohitadmin <rohitkasera1993@gmail.com>
2019-04-09 20:34:10 +0530

    commited

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListsComponent } from './recipes/recipe-lists/recipe-lists.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-lists/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListsComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/recipes/recipe-lists/recipe-lists.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-lists',
+  templateUrl: './recipe-lists.component.html',
+  styleUrls: ['./recipe-lists.component.css']
+})
+export class RecipeListsComponent implements OnInit {
+  recipes: Recipe[] = [
+   new Recipe('Chicken Birayani', 'Best Chicken to in the Birayani industry, just smell and eat it.', 'https://banner2.kisspng.com/20180330/vkq/kisspng-fizzy-drinks-hyderabadi-biryani-chicken-65-biryani-5abedc78a680a8.782596021522457720682.jpg')
+  ];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
